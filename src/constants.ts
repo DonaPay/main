dbd859f68e997753da9655bd0b9a182d20fe1f56
Raghav <raghavlathi0003@gmail.com>
2024-10-13 19:43:37 +0530

    Updated new publisher address

--- src/constants.ts
+++ src/constants.ts
@@ -3,4 +3,4 @@ import type { Network } from "@aptos-labs/wallet-adapter-react";
 export const NETWORK: Network = (process.env.NEXT_PUBLIC_APP_NETWORK as Network) ?? "testnet";
 export const MODULE_ADDRESS = process.env.NEXT_PUBLIC_MODULE_ADDRESS;
 
-export const PUBLISHER_ADDRESS = "071af5095e2ec809ce2998185f802ee2d4d936401614cc71a3987cc5a341a5ff";
+export const PUBLISHER_ADDRESS = "0x094468b85e81bafd35d320a3a8f8dcc4269635368cfa807d534405eee6e0e58a";
